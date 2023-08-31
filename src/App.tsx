1d5a6567a7ed37f5f79797b50181562a12d1ccf5
ovovvvvv <ovovvvvv@naver.com>
2023-08-31 17:55:23 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-slate-800 mx-10">삼육대학교 2023 축제 페이지</h1>
+    </>
+  );
+}
+
+export default App;
