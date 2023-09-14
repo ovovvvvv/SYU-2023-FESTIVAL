a62d4033d58a7c370b859e622e4304a6ea51d06e
ovovvvvv <ovovvvvv@naver.com>
2023-09-14 20:27:42 +0900

    텍스트 애니메이션 오류 수정

--- src/components/ui/timeline/TimeTableBox.tsx
+++ src/components/ui/timeline/TimeTableBox.tsx
@@ -6,12 +6,15 @@ interface TimeTableBoxProps {
 const TimeTableBox = ({ time, activity }: TimeTableBoxProps) => {
   return (
     <div>
-      <div className="flex flex-row justify-start space-x-5 text-sm">
-        <p className="mx-6">{time}</p>
-        <p>{activity}</p>
+      <div
+        className="flex flex-row justify-start space-x-5 text-sm
+      "
+      >
+        <p className="mx-6  Pretendard-Regular">{time}</p>
+        <p className="mx-6 Pretendard-SemiBold ">{activity}</p>
       </div>
       <div className="flex flex-col items-center">
-        <div className=" border-t w-[322px] border-[#CECECE] my-2"></div>
+        <div className="border-t w-[322px] border-[#CECECE] my-2"></div>
       </div>
     </div>
   );
--- src/pages/TimeTablePage.tsx
+++ src/pages/TimeTablePage.tsx
@@ -3,7 +3,7 @@ import './TimeTablePage.css';
 
 const TimeTablePage = () => {
   return (
-    <div className="bg-timetable h-screen overflow-hidden">
+    <div className="bg-timetable h-screen">
       <div className="flex flex-col justify-center items-center ">
         <div
           className="text-[#fff] border-2 p-2 my-5"
@@ -11,14 +11,29 @@ const TimeTablePage = () => {
         >
           TIME-TABLE
         </div>
-        <div className="flex items-center justify-center text-sm w-[352px] h-[38px] rounded-t-lg bg-black text-[#fff]">
+        <div className="flex items-center justify-center text-sm w-[342px] h-[38px] rounded-t-lg bg-black text-[#fff]">
           부스 시간표
         </div>
-        <div className="w-[350px] h-[226px] flex flex-col justify-center bg-[#FAFAFA]">
+        <div className="w-[340px] h-[226px] flex flex-col justify-center bg-[#FAFAFA] rounded-b-lg">
           <TimeTableBox time="09:00 - 17:00" activity="부스활동 진행" />
           <TimeTableBox time="17:00 - 18:00" activity="핫 타임(할인) 진행" />
           <TimeTableBox time="18:00 - 21:00" activity="야간 운영 전환" />
           <TimeTableBox time="21:00 - 22:00" activity="부스 마무리 및 정리" />
+        </div>
+        <div className="flex items-center justify-center text-sm w-[342px] h-[38px] mt-5 rounded-t-lg bg-black text-[#fff]">
+          행사 진행 순서
+        </div>
+        <div className="w-[340px] h-[430px] flex flex-col justify-center bg-[#FAFAFA] rounded-b-lg">
+          <TimeTableBox time="16:30 - 17:30" activity="개회식 + 레크레이션" />
+          <TimeTableBox time="17:30 - 19:00" activity="밴드 공연" />
+          <TimeTableBox time="19:00 - 19:15" activity="레크레이션 1" />
+          <TimeTableBox time="19:15 - 19:45" activity="댄스동아리 공연" />
+          <TimeTableBox time="19:45 - 20:00" activity="레크레이션 2" />
+          <TimeTableBox time="20:00 - 20:30" activity="A 공연자 공연" />
+          <TimeTableBox time="20:30 - 21:00" activity="B 공연자 공연" />
+          <TimeTableBox time="21:00 - 21:30" activity="C 공연자 공연" />
+          <TimeTableBox time="21:30 - 21:55" activity="EDM 공연 및 퇴장 안내" />
+          <TimeTableBox time="21:55 - 22:00" activity="마무리" />
         </div>
       </div>
     </div>
