387b809554ace88b4cf37c72bbf823bd2677bc70
ovovvvvv <ovovvvvv@naver.com>
2023-09-15 23:38:08 +0900

    애니메이션 수정

--- src/pages/TimeTablePage.tsx
+++ src/pages/TimeTablePage.tsx
@@ -10,11 +10,12 @@ const TimeTablePage = forwardRef((_, ref: ForwardedRef<HTMLDivElement>) => {
     >
       <div className="flex flex-col justify-center items-center ">
         <div
-          className="text-[#fff] border-2 p-2 my-5"
+          className="text-[#fff] border-2 p-2 my-8"
           style={{ borderRadius: '50%', fontFamily: 'Montserrat, sans-serif' }}
         >
           TIME-TABLE
         </div>
+
         <div
           className=" flex items-center justify-center text-sm w-[342px] h-[38px] rounded-t-lg bg-black text-[#fff] text-semibold"
           style={{ fontFamily: 'Pretendard-Semibold' }}
@@ -30,26 +31,31 @@ const TimeTablePage = forwardRef((_, ref: ForwardedRef<HTMLDivElement>) => {
           <TimeTableBox time="18:00 - 21:00" activity="야간 운영 전환" />
           <TimeTableBox time="21:00 - 22:00" activity="부스 마무리 및 정리" />
         </div>
-        <div
-          className="flex items-center justify-center text-sm w-[342px] h-[38px] mt-5 rounded-t-lg bg-black text-[#fff]"
-          style={{ fontFamily: 'Pretendard-Semibold' }}
-        >
-          행사 진행 순서
-        </div>
-        <div
-          className="w-[340px] h-[430px] flex flex-col justify-center bg-[#FAFAFA] rounded-b-lg"
-          style={{ fontFamily: 'Pretendard-Semibold' }}
-        >
-          <TimeTableBox time="16:30 - 17:30" activity="개회식 + 레크레이션" />
-          <TimeTableBox time="17:30 - 19:00" activity="밴드 공연" />
-          <TimeTableBox time="19:00 - 19:15" activity="레크레이션 1" />
-          <TimeTableBox time="19:15 - 19:45" activity="댄스동아리 공연" />
-          <TimeTableBox time="19:45 - 20:00" activity="레크레이션 2" />
-          <TimeTableBox time="20:00 - 20:30" activity="A 공연자 공연" />
-          <TimeTableBox time="20:30 - 21:00" activity="B 공연자 공연" />
-          <TimeTableBox time="21:00 - 21:30" activity="C 공연자 공연" />
-          <TimeTableBox time="21:30 - 21:55" activity="EDM 공연 및 퇴장 안내" />
-          <TimeTableBox time="21:55 - 22:00" activity="마무리" />
+        <div className="my-5">
+          <div
+            className="flex items-center justify-center text-sm w-[342px] h-[38px] rounded-t-lg bg-black text-[#fff]"
+            style={{ fontFamily: 'Pretendard-Semibold' }}
+          >
+            행사 진행 순서
+          </div>
+          <div
+            className="w-[340px] h-[430px] flex flex-col justify-center bg-[#FAFAFA] rounded-b-lg"
+            style={{ fontFamily: 'Pretendard-Semibold' }}
+          >
+            <TimeTableBox time="16:30 - 17:30" activity="개회식 + 레크레이션" />
+            <TimeTableBox time="17:30 - 19:00" activity="밴드 공연" />
+            <TimeTableBox time="19:00 - 19:15" activity="레크레이션 1" />
+            <TimeTableBox time="19:15 - 19:45" activity="댄스동아리 공연" />
+            <TimeTableBox time="19:45 - 20:00" activity="레크레이션 2" />
+            <TimeTableBox time="20:00 - 20:30" activity="A 공연자 공연" />
+            <TimeTableBox time="20:30 - 21:00" activity="B 공연자 공연" />
+            <TimeTableBox time="21:00 - 21:30" activity="C 공연자 공연" />
+            <TimeTableBox
+              time="21:30 - 21:55"
+              activity="EDM 공연 및 퇴장 안내"
+            />
+            <TimeTableBox time="21:55 - 22:00" activity="마무리" />
+          </div>
         </div>
       </div>
     </div>
