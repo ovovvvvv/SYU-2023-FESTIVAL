de17543e4cfdd7491638d992164219afde55f724
ovovvvvv <ovovvvvv@naver.com>
2023-09-17 00:40:20 +0900

    #2 헤더 애니메이션 무한 슬라이드 수정

--- src/components/ui/Intro/HeaderAnimation.tsx
+++ src/components/ui/Intro/HeaderAnimation.tsx
@@ -2,14 +2,24 @@ import './TextAnimation.css';
 
 const HeaderAnimation = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col">
       {/* 상, 하단 애니메이션 텍스트 */}
-      <h1 className="text-flow absolute top-1 traking-wide text-4xl text-white whitespace-nowrap">
-        SAHMYOOK UNIVERSITY
-      </h1>
-      <h1 className="text-flow absolute bottom-[20px] tracking-wide text-4xl text-white whitespace-nowrap">
-        SYU FESTIVAL
-      </h1>
+      <div className="flex flex-row animate-slider absolute top-1 traking-wide text-4xl text-white whitespace-nowrap">
+        <h1 className="mx-2">SAHMYOOK UNIVERSITY</h1>
+        <h1 className="mx-2">SAHMYOOK UNIVERSITY</h1>
+        <h1 className="mx-2">SAHMYOOK UNIVERSITY</h1>
+        <h1 className="mx-2">SAHMYOOK UNIVERSITY</h1>
+        <h1 className="mx-2">SAHMYOOK UNIVERSITY</h1>
+        <h1 className="mx-2">SAHMYOOK UNIVERSITY</h1>
+      </div>
+      <div className="flex flex-row animate-slider absolute bottom-[20px] tracking-wide text-4xl text-white whitespace-nowrap">
+        <h1 className="mx-2">SYU FESTIVAL</h1>
+        <h1 className="mx-2">SYU FESTIVAL</h1>
+        <h1 className="mx-2">SYU FESTIVAL</h1>
+        <h1 className="mx-2">SYU FESTIVAL</h1>
+        <h1 className="mx-2">SYU FESTIVAL</h1>
+        <h1 className="mx-2">SYU FESTIVAL</h1>
+      </div>
     </div>
   );
 };
