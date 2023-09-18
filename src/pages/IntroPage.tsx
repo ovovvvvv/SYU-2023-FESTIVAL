87897542368b756d494fa9091aab59b6af06c382
ovovvvvv <ovovvvvv@naver.com>
2023-09-18 18:41:26 +0900

    vercel.json

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -5,9 +5,17 @@ import FlowerAnimation from '../components/ui/Intro/FlowerAnimation';
 const IntroPage = () => {
   return (
     <div>
-      <div className="bg-1ptitle h-screen flex flex-col justify-center overflow-hidden">
+      <div className="bg-titleimg h-screen flex flex-col justify-center overflow-hidden">
         {/* 상, 하단 애니메이션 텍스트 */}
         <HeaderAnimation />
+        <div className="absolute bottom-[20%] left-1/2 transform -translate-x-1/2 text-[#fff] ">
+          <p
+            className="tracking-[10px] whitespace-nowrap"
+            style={{ fontFamily: 'GmarketSansBold' }}
+          >
+            SCROLL-DOWN
+          </p>
+        </div>
 
         {/* 꽃 애니메이션 */}
         <FlowerAnimation />
