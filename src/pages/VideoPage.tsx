2d86fbf410582665acf72c9167071a241df7f65c
ovovvvvv <ovovvvvv@naver.com>
2023-09-30 00:28:19 +0900

    #9 비디오 첨부

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -4,12 +4,16 @@ const VideoPage = forwardRef((_, ref: ForwardedRef<HTMLDivElement>) => {
   return (
     <div ref={ref} className="flex flex-col items-center text-[#fff] py-[70%]">
       <h1
-        className="text-[25px] underline underline-offset-8 mb-6"
+        className="text-[25px] underline underline-offset-8 mb-16"
         style={{ fontFamily: 'Pretendard-semibold' }}
       >
         축전영상
       </h1>
-      <div className="">축전영상이 들어갑니다 ㅎㅎ</div>
+      <div className="border-[3px] rounded-xl p-2">
+        <video controls preload="metadata" className="w-[340px]">
+          <source src="/video/festival.mp4#t=0.7" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 });
